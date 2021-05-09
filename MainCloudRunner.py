7a925b6af0a33186f8f232e41f239a89337233ae
mrru <esgems@gmail.com>
2021-05-09 18:53:27 +0300

    fixed code, updated accuracy

--- MainCloudRunner.py
+++ MainCloudRunner.py
@@ -11,7 +11,3 @@ tfc.run(
 			accelerator_type=tfc.AcceleratorType.NVIDIA_TESLA_P100,
 			accelerator_count=2),
 	worker_count=0)
-
-# gsutil -m cp "gs://projectcatsbucket/checkpoints/checkpoint" "gs://projectcatsbucket/checkpoints/train_60.tf.data-00000-of-00001" "gs://projectcatsbucket/checkpoints/train_60.tf.index" .
-# gsutil -m cp "gs://projectcatsbucket/saved_models/modelVgg/checkpoint" "gs://projectcatsbucket/saved_models/modelVgg/modelVgg.data-00000-of-00001" "gs://projectcatsbucket/saved_models/modelVgg/modelVgg.index" .
-# gsutil -m cp "gs://projectcatsbucket/saved_models/modelVgg/checkpoint" "gs://projectcatsbucket/saved_models/modelVgg/modelVgg.data-00000-of-00001" "gs://projectcatsbucket/saved_models/modelVgg/modelVgg.index" .
